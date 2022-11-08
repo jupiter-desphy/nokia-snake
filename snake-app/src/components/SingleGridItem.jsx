db3558bbe30159b47ea391a250dca8ef1ed70cae
Jupiter Desphy <jupiterdesphy7@gmail.com>
2022-11-08 14:55:21 -0800

    snake gets full on corners now working

--- snake-app/src/components/SingleGridItem.jsx
+++ snake-app/src/components/SingleGridItem.jsx
@@ -2,10 +2,10 @@ export default function Tile({pixelState}){
     return (
         <div className={`pixel ${pixelState}`}>
 
-            <div className="d-f d-h r-h r-f"></div>
+            <div className="f-s-e d-f d-h r-h r-f"></div>
             <div className="f l-f s-w s-e u-t up down food-pixel"></div>
             <div className="f d-f r-f s-w s-e d-h d-t u-t up down"></div>
-            <div className="l-f l-h"></div>
+            <div className="f-s-w l-f l-h"></div>
 
             <div className="f u-f s-e n-e l-t left right food-pixel"></div>
             <div className="d-f r-f s-w n-e u-t u-h d-h l-h l-t r-h right down"></div>
@@ -17,25 +17,10 @@ export default function Tile({pixelState}){
             <div className="u-f l-f s-w n-e d-h u-h u-t d-t l-h l-t r-t r-h down right"></div>
             <div className="f d-f l-f s-w n-w l-h l-t r-t right left"></div>
 
-            <div className="u-f u-h"></div>
+            <div className="f-n-e u-f u-h"></div>
             <div className="f l-f n-w n-e d-t up down"></div>
             <div className="f u-f r-f n-w n-e u-h u-t d-t up down"></div>
-            <div className=""></div>
+            <div className="f-n-w"></div>
         </div>
     )
 }
-// list out different things this could display
-/**
- * 3 x 3 cells
- * single grid item itself to be identified by [x, y] coordinates
- * states: snake, food, head or blank
- * 
- */
-
-/* snake array should look like 
-    snakeArr = [
-        [0,0],
-        [1,0],
-        [2,0]
-    ]
-*/
